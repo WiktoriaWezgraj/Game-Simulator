0768ee0159fa6b6448f794b4514753ab95919c5e
WiktoriaWezgraj <wiktoriawezgraj1@gmail.com>
2024-10-29 22:19:45 +0100

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -1,16 +1,36 @@
 ﻿using System;
 
 namespace Simulator;
-
 public class Animals
 {
-    public required string Description { get; init; } = "Dogs";
-    public uint Size { get; set; } = 3;
-    //public Animals(string description, uint size)
-    //{
-    //    Description = description;
-    //    Size = size;
-    //}
+    private string _description = "Unknown description";
+    public required string Description
+    {
+        get => _description;
+        set
+        {
+            if (_description != "Unknown description") //Sprawdzenie czy wartość została już nadana przy inicjacji, jeśli tak to funkcja wraca pusta, co nie pozwala na kolejną zmianę
+                return;
 
-    public string Info() => $"{Description} <{Size}>";
+            value = value.Trim();
+
+            if (value.Length < 3)
+                value = value.PadRight(3, '#');
+
+            if (value.Length > 15)
+            {
+                value = value.Substring(0, 15).TrimEnd();
+                if (value.Length < 3)
+                    value = value.PadRight(3, '#');
+            }
+
+            if (char.IsLower(value[0]))
+                value = char.ToUpper(value[0]) + value.Substring(1);
+
+            _description = value;
+        }
+    }
+    public uint Size { get; set; } = 3;
+
+    public string Info => $"{Description} <{Size}>";
 }
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -1,23 +1,70 @@
-﻿using System;
+﻿namespace Simulator;
 
 public class Creature
 {
+    private string _name = "Unknown";
+    private int _level = 1;
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            if (_name != "Unknown") //Sprawdzenie czy wartość została już nadana przy inicjacji, jeśli tak to funkcja wraca pusta, co nie pozwala na kolejną zmianę
+                return;
 
-    public string Name { get; set; }
-    public int Level { get; set; }
+            value = value.Trim();
+
+            if (value.Length < 3)
+                value = value.PadRight(3, '#');
+
+            if (value.Length > 25)
+            {
+                value = value.Substring(0, 25).TrimEnd();
+                if (value.Length < 3)
+                    value = value.PadRight(3, '#');
+            }
+
+            if (char.IsLower(value[0]))
+                value = char.ToUpper(value[0]) + value.Substring(1);
+
+            _name = value;
+        }
+    }
+    public int Level
+    {
+        get => _level;
+        set
+        {
+            if (_level != 1) 
+                return;
+
+            if (value < 1)
+                value = 1;
+            else if (value > 10)
+                value = 10;
+
+            _level = value;
+        }
+    }
 
     public Creature(string name, int level = 1)
     {
         Name = name;
         Level = level;
     }
+
     public Creature() { }
 
-    //public string Info => $"{Name} <Level {Level}>";
-    public string Info() { return $"{Name} {Level}";}
+    public string Info => $"{Name} <{Level}>";
 
     public void SayHi()
     {
-        Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+        Console.WriteLine($"Hi, my name is {Name} and I am at level {Level}");
+    }
+
+    public void Upgrade()
+    {
+        if (_level < 10)
+            _level++;
     }
 }
